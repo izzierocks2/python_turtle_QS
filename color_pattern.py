a8ea519b4feb26a86393137facb1af70c4626e2d
Grace <gracek2705@tuckahoeschools.org>
2024-10-29 08:17:26 -0400

    Update color_pattern.py

--- color_pattern.py
+++ color_pattern.py
@@ -1,1 +1,11 @@
+import turtle
+t = turtle.Turtle()
+t.shape(name="turtle")
+t.speed(0)
 
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple']
+
+for i in range(500):
+  t.forward(i)
+  t.color(colors[i % len(colors)])
+  t.left(50)
